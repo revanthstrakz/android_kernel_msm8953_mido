231be83ac9e0b1de5b92e0792a6a9ba9aa85b4ba
Ken Xue <ken.xue@amd.com>
2015-12-01 14:45:23 +0800

    Revert "SCSI: Fix NULL pointer dereference in runtime PM"
    [ Upstream commit 1c69d3b6eb73e466ecbb8edaf1bc7fd585b288da ]
    
    This reverts commit 49718f0fb8c9 ("SCSI: Fix NULL pointer dereference in
    runtime PM")
    
    The old commit may lead to a issue that blk_{pre|post}_runtime_suspend and
    blk_{pre|post}_runtime_resume may not be called in pairs.
    
    Take sr device as example, when sr device goes to runtime suspend,
    blk_{pre|post}_runtime_suspend will be called since sr device defined
    pm->runtime_suspend. But blk_{pre|post}_runtime_resume will not be called
    since sr device doesn't have pm->runtime_resume. so, sr device can not
    resume correctly anymore.
    
    More discussion can be found from below link.
    http://marc.info/?l=linux-scsi&m=144163730531875&w=2
    
    Signed-off-by: Ken Xue <Ken.Xue@amd.com>
    Acked-by: Alan Stern <stern@rowland.harvard.edu>
    Cc: Xiangliang Yu <Xiangliang.Yu@amd.com>
    Cc: James E.J. Bottomley <JBottomley@odin.com>
    Cc: Jens Axboe <axboe@kernel.dk>
    Cc: Michael Terry <Michael.terry@canonical.com>
    Cc: stable@vger.kernel.org
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    (cherry picked from commit 1c857dc0e69fdba81e5169cf4b3d2a9daaa783eb)

--- drivers/scsi/scsi_pm.c
+++ drivers/scsi/scsi_pm.c
@@ -262,9 +262,13 @@ static int sdev_runtime_suspend(struct device *dev)
 		err = blk_pre_runtime_suspend(sdev->request_queue);
 		if (err)
 			return err;
+	err = blk_pre_runtime_suspend(sdev->request_queue);
+	if (err)
+		return err;
+	if (pm && pm->runtime_suspend)
 		err = pm->runtime_suspend(dev);
-		blk_post_runtime_suspend(sdev->request_queue, err);
-	}
+	blk_post_runtime_suspend(sdev->request_queue, err);
+
 	return err;
 }
 
@@ -292,9 +296,11 @@ static int sdev_runtime_resume(struct device *dev)
 
 	if (pm && pm->runtime_resume) {
 		blk_pre_runtime_resume(sdev->request_queue);
+	blk_pre_runtime_resume(sdev->request_queue);
+	if (pm && pm->runtime_resume)
 		err = pm->runtime_resume(dev);
-		blk_post_runtime_resume(sdev->request_queue, err);
-	}
+	blk_post_runtime_resume(sdev->request_queue, err);
+
 	return err;
 }
 
