4a54c2ee65ab8541d5c0c4901364464ff8ec7cdf
TheHitMan7 <krtik.vrma@gmail.com>
2018-11-11 01:59:41 +0530

    Fix conflict: lib: make memzero_explicit more robust against dead store elimination
    Signed-off-by: TheHitMan7 <krtik.vrma@gmail.com>

--- include/linux/compiler-gcc.h
+++ include/linux/compiler-gcc.h
@@ -8,7 +8,7 @@
 #define GCC_VERSION (__GNUC__ * 10000		\
 		     + __GNUC_MINOR__ * 100	\
 		     + __GNUC_PATCHLEVEL__)
-=======
+
 #define GCC_VERSION (__GNUC__ * 10000 \
 		   + __GNUC_MINOR__ * 100 \
 		   + __GNUC_PATCHLEVEL__)
