f437ded606422c3fc9f8a924946b2e01794891e5
TheHitMan7 <krtik.vrma@gmail.com>
2019-01-29 15:06:21 +0530

    Update: Kernel build script
    Signed-off-by: TheHitMan7 <krtik.vrma@gmail.com>

--- build.sh
+++ build.sh
@@ -1,46 +1,45 @@
 #!/bin/bash
+#
+# File Type      : Kernel Build Script
+# Github         : TheHitMan7 <krtik.vrma@gmail.com>
+# Build Date     : Monday October 1, 2018
+# xda-developers : TheHitMan
 
-#####################################
-#           Revolt Kernel           #
-#                                   #
-#   My Basic Kernel Build Script    #
-#  TheHitMan7<krtik.vrma@gmail.com> #
-#       Monday October 1, 2018      #
-#     Wednesday December 5, 2018    #
-#####################################
+#
+# // main //
+# // Revolt Revolution //
+#
 
-##################
-#     Revolt     #
-##################
-BUILD="~/android/kernel/android_kernel_msm8953_mido"
 # Set kernel source workspace
+BUILD=""
 cd $BUILD
 # Export ARCH <arm, arm64, x86, x86_64>
-export ARCH=arm64
+export ARCH=
 # Export SUBARCH <arm, arm64, x86, x86_64>
-export SUBARCH=arm64
+export SUBARCH=
 # Set kernal name
-export LOCALVERSION=Revolt.EAS-CAF.Pie.rX
-# Export Username
-export KBUILD_BUILD_USER=TheHitMan
-# Export Machine name
-export KBUILD_BUILD_HOST=CINNAMON
-# Compiler String
-CC="~/kernel/clang-r344140b/bin/clang"
+export LOCALVERSION=
+# Set Username
+export KBUILD_BUILD_USER=
+# Set Machine name
+export KBUILD_BUILD_HOST=
+# Set Compiler String
+CC=""
 export KBUILD_COMPILER_STRING="$(${CC} --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')"
 # Make and Clean
 make clean && make mrproper
 # Make <defconfig>
-make mido_defconfig
+DEFCONFIG=""
+make $DEFCONFIG
 # Build Kernel
-make ARCH=arm64 \
-CC=~/kernel/clang-r344140b/bin/clang \
+make ARCH= \
+CC= \
 CLANG_TRIPLE=aarch64-linux-gnu- \
-CROSS_COMPILE=~/kernel/aarch64-linux-android-4.9/bin/aarch64-linux-android- \
--j4
+CROSS_COMPILE= \
+-jX
 # Make kernel zip
-KERNEL_DIR="~/android/kernel/android_kernel_msm8953_mido"
-ZIP_DIR="~/Revolt.EAS-CAF.Pie.rX"
+KERNEL_DIR=""
+ZIP_DIR=""
 cp $KERNEL_DIR/arch/arm64/boot/dts/qcom/msm8953-qrd-sku3-mido-nontreble.dtb $ZIP_DIR/treble-unsupported/
 cp $KERNEL_DIR/arch/arm64/boot/dts/qcom/msm8953-qrd-sku3-mido-treble.dtb $ZIP_DIR/treble-supported/
 cp $KERNEL_DIR/arch/arm64/boot/Image.gz $ZIP_DIR/kernel/
